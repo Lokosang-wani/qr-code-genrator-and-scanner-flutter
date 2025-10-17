22597cde6972c00650b1a28d2a68fc8f7401283b
Felix Wani <lokosangwani21@Gmail.com>
2025-10-18 01:12:13 +0200

    addd forgotten qr camera-scanner screen

--- lib/scan_qr_code.dart
+++ lib/scan_qr_code.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+import 'package:qr_code_generator_and_scanner/qr_scanner_screen.dart';
 
 class ScanQrCode extends StatefulWidget {
   const ScanQrCode({super.key});
@@ -8,10 +11,29 @@ class ScanQrCode extends StatefulWidget {
 }
 
 class _ScanQrCodeState extends State<ScanQrCode> {
+  String qrResult = 'Scanned Data will appear here';
+  Future<void> scanQR() async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => QrScannerScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Scan Qr Code'), backgroundColor: Colors.blue),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 30),
+            Text('$qrResult', style: TextStyle(color: Colors.black)),
+            SizedBox(height: 30),
+            ElevatedButton(onPressed: scanQR, child: Text('Scan Code')),
+          ],
+        ),
+      ),
     );
   }
 }
